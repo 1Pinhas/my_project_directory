1959f5ee016798b3a39eae04cd276e1476dabc10
1Pinhas <ngayepinhas@gmail.com>
2024-10-14 08:05:02 +0100

    mise en place des methodes du controllers clients

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -10,11 +10,49 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class ClientController extends AbstractController
 {
-    #[Route('/client', name: 'app_client')]
+    #[Route('/clients', name: 'clients.index', methods:['GET'])]
     public function index(Request $request): Response
     {
         return $this->render('client/list.html.twig', [
             'controller_name' => 'ClientController',
         ]);
     }
+
+    //utilsationn des path variables(les données en parametres)
+    #[Route('/clients/show/{id?}', name: 'clients.show', methods:['GET'])]
+    public function show(int $id): Response
+    {
+        return $this->render('client/list.html.twig', [
+            'controller_name' => 'ClientController',
+        ]);
+    }
+
+    //utilisatin des query params
+    #[Route('/clients/search/phone', name: 'clients.showClientByPhone', methods:['GET'])]
+    public function searchClientByPhone(Request $request): Response
+    {
+        //$request->query->get('phone')=>$_GET['key];
+        //$request->request->get('name_field') => $_POST['name_field']
+        $phone = $request->query->get('tel');
+        return $this->render('client/list.html.twig', [
+            'controller_name' => 'ClientController',
+        ]);
+    }
+
+    #[Route('/clients/remove/{id?}', name: 'clients.remove', methods:['GET', 'POST'])]
+    public function remove(int $id): Response
+    {
+        return $this->render('client/list.html.twig', [
+            'controller_name' => 'ClientController',
+        ]);
+    }
+
+    #[Route('/clients/store', name: 'clients.store', methods:['GET', 'POST'])]
+    public function store(): Response
+    {
+        return $this->render('client/list.html.twig', [
+            'controller_name' => 'ClientController',
+        ]);
+    }
+
 }
